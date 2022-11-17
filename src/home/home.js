bf202a22ad2b27eb61d163319bda84daf58ab364
vishnu-inqui <vishnu@inqui-lab.org>
2022-11-17 12:36:34 +0530

    updated telangana map

--- src/home/home.js
+++ src/home/home.js
@@ -66,7 +66,7 @@ import ScrollToTop from 'react-scroll-to-top';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { getSchedulesForTeacherAndStudents } from '../redux/schedules/actions';
 import { compareDates } from '../helpers/Utils';
-import KarnatakaMap from '../components/MapCard/KarnatakaMap';
+import TelanganaMap from '../components/MapCard/TelanganaMap';
 
 const Home = () => {
     const { t } = useTranslation();
@@ -851,7 +851,7 @@ const Home = () => {
                         {t('home_tl.engagement')}
                     </h2>
                 </div>
-                <KarnatakaMap />
+                <TelanganaMap />
             </section>
 
             {/* <section className="blog">
